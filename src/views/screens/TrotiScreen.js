1936f133e55624a81e93112c7fc9648c7d5532bc
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-02-01 15:52:22 +0100

    i just make 3 flexbox

--- src/views/screens/TrotiScreen.js
+++ src/views/screens/TrotiScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableOpacity, Text, Image} from 'react-native';
+import BackIcon from 'react-native-vector-icons/Feather';
+import LoginScreen from './LoginScreen';
 
-const TrotiScreen = () => {
+const TrotiScreen = ({navigation}) => {
   return (
     <View
       style={[
@@ -11,9 +13,19 @@ const TrotiScreen = () => {
           flexDirection: 'column',
         },
       ]}>
-      <View style={{flex: 1, backgroundColor: 'red'}} />
-      <View style={{flex: 5, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 2, backgroundColor: 'green'}} />
+        <View style={{flex: 1.2, backgroundColor: 'white'}}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.push('Test')}>
+        <BackIcon name="chevron-left" size={47} color="white"></BackIcon>
+        
+      </TouchableOpacity>
+        </View>
+        <View style={{flex: 5, backgroundColor: 'white'}}>
+        <Image style={styles.Image} source={require('../../img/scooter.png')}/>
+          
+        </View>
+        <View style={{flex: 2, backgroundColor: '#45DE88'}}>
+
+        </View>
     </View>
   );
 };
@@ -22,6 +34,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    backgroundColor: "#817D7D",
+    borderRadius: 10,
+    height: 50,
+    width: 50,
+    marginTop: 50,
+    marginLeft: 24,
+  },
+  image: {
+
+  }
 });
 
 export default TrotiScreen;
