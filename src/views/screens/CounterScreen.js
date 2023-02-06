af71fe6dd32b9d9d497bbbf59c1141091ee76d30
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-02-06 19:14:32 +0100

    i just add counter page

--- src/views/screens/CounterScreen.js
+++ src/views/screens/CounterScreen.js
@@ -8,7 +8,29 @@ import { View, Text, StyleSheet } from 'react-native';
 const CounterScreen = () => {
     return (
         <View style={styles.container}>
-            <View></View>
+            <View style={styles.top}>
+                <View style={styles.countext}>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold'}}>DD</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold'}}>/</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold' }}>HH</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold' }}>/</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold' }}>MM</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold' }}>/</Text>
+                <Text style={{fontSize: 30, color: 'grey', fontFamily: 'bold' }}>SS</Text>
+                </View>
+                <View style={styles.countumber}>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>22</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>:</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>10</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>:</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>12</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>:</Text>
+                <Text style={{fontSize: 30, color: '#45DE88' }}>33</Text>
+                </View>
+            </View>
+            <View style={styles.bottom}>
+
+            </View>
         </View>
     );
 };
@@ -17,10 +39,27 @@ const CounterScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: 'white',
     },
+    top: {
+        flex: 5,
+        
+    },
+    countext:{
+        flexDirection:'row',
+        justifyContent: 'space-evenly',
+        marginTop: 170 
+    },
+    countumber: {
+        flexDirection:'row',
+        justifyContent: 'space-evenly',
+        marginTop: 25
+    },
+    bottom: {
+        flex: 2,
+        backgroundColor: "red"
+    }
+
 });
 
 //make this component available to the app
